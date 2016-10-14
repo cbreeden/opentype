c782284f1cb6323b66d9a89b42446559c14a7f11
Christopher Breeden <github@u.breeden.cc>
2016-10-14 17:58:00 -0500

    Rename variants fields.

--- src/mathematics/variants.rs
+++ src/mathematics/variants.rs
@@ -2,10 +2,10 @@ table! {
     #[doc = "A table of varaint glyphs to satisfy alternate measurements requirements."]
     #[derive(Copy)]
     pub Variants { // MathVariants
-        min_connector_overlap (i16),
-        vert_coverage_offset  (u16), // VertGlyphCoverage
-        horz_coverage_offset  (u16), // HorizGlyphCoverage
-        vert_count            (u16), // VertGlyphCount
-        horz_count            (u16), // HorizGlyphCount
+        min_connector_overlap      (i16),
+        vertical_coverage_offset   (u16), // VertGlyphCoverage
+        horizontal_coverage_offset (u16), // HorizGlyphCoverage
+        vertical_count             (u16), // VertGlyphCount
+        horizontal_count           (u16), // HorizGlyphCount
     }
 }
